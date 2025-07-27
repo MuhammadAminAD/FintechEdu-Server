9edcf6a46c7261e061632638213599148004c4e3
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-07-27 19:15:20 +0500

    spelling error corrected (register -> registration)

--- src/router/route.js
+++ src/router/route.js
@@ -3,9 +3,9 @@ import RegisterController from "../controllers/auth/Register/register.js";
 
 const router = Router();
 
-router.post("/auth/regsiter/step1", (req, res) => RegisterController.Step1(req, res));
-router.post("/auth/regsiter/step2", (req, res) => RegisterController.Step2(req, res));
-router.post("/auth/regsiter/step3", (req, res) => RegisterController.Step3(req, res));
-router.post("/auth/regsiter/step4", (req, res) => RegisterController.Step4(req, res));
+router.post("/auth/registration/step1", (req, res) => RegisterController.Step1(req, res));
+router.post("/auth/registration/step2", (req, res) => RegisterController.Step2(req, res));
+router.post("/auth/registration/step3", (req, res) => RegisterController.Step3(req, res));
+router.post("/auth/registration/step4", (req, res) => RegisterController.Step4(req, res));
 
 export default router;
